c89eeda4aabc29829dd1f5c391a2f617899e163c
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-12-08 04:49:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,3 +9,19 @@ class Application
   end
  
 end
+
+
+def call(env)
+    resp = Rack::Response.new
+
+    if Time.now.hour.between?(0, 11)
+      resp.write "Good Morning!"
+    elsif Time.now.hour.between?(12, 17)
+      resp.write "Good Afternoon."
+    else
+      resp.write "Good Evening!"
+    end
+
+    resp.finish
+  end
+end
